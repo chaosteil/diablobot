0d5b3fa25557cce640be74ba47784fc1bac32f83
listen2 <dog_pelican@yahoo.com>
2012-03-24 14:01:21 -0400

    Make !rules show who triggered it, if sending to another user.

--- DiabloBasic/plugin.py
+++ DiabloBasic/plugin.py
@@ -191,7 +191,7 @@ class DiabloBasic(callbacks.Plugin):
             #if not irc.state.channels["#diablo"].isOp(msg.nick):
             #    irc.reply("Only operators can tell rules to others.", private=True)
             #    return
-            pass
+            irc.reply("This message was triggered by %s" % msg.nick, private=True, to=victim)
         else:
             victim = msg.nick
         for v in DiabloCommon.channel_rules:
