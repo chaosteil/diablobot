07a9dde88e07508dda1563a322759f3acc2b417e
listen2 <dog_pelican@yahoo.com>
2012-05-02 19:26:48 -0400

    Use a slightly shorter "no quotes" message.

--- DiabloBasic/plugin.py
+++ DiabloBasic/plugin.py
@@ -102,7 +102,7 @@ class DiabloBasic(callbacks.Plugin):
 
         # Help text
         elif charname not in self.quotes:
-            irc.reply("I don't have any quotes from %s. Available quote sources: %s (%d quotes)" % \
+            irc.reply("I don't have any quotes from %s. Available sources: %s (%d quotes)" % \
                 (charname, ", ".join(sorted(self.quotes.keys())), self.quote_count))
 
         # Prints a quote of the character
